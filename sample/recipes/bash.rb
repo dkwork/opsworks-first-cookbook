8645e48c8c5908123a404071c525e12350b99c9d
Djura <dk@storebox.com>
2017-07-04 12:58:26 +0200

    /DK

--- sample/recipes/bash.rb
+++ sample/recipes/bash.rb
@@ -1,6 +1,7 @@
-bash "change system greeting" do
+bash "change system greeting" 
+do
   user "root"
   code <<-EOH
-  echo "Hello OpsWorks World" > /etc/motd
+  echo "Hello OpsWorks World" >> /etc/motd
   EOH
 end
