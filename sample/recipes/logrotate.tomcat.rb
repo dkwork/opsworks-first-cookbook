cfbb6dc4cbcf89905fb6ed7f253abbf5894060b0
Djura <dk@storebox.com>
2017-07-05 14:29:55 +0200

    /DK

--- sample/recipes/logrotate.tomcat.rb
+++ sample/recipes/logrotate.tomcat.rb
@@ -1,16 +1,26 @@
 bash "change Logrotate.conf" do
   user "root"
   code <<-EOH
-  #vDATE=`date '+%Y%m%d'`
-  #/etc/logrotate.elasticbeanstalk.hourly/logrotate.elasticbeanstalk.tomcat8.conf
-  vFIL="/etc/logrotate.elasticbeanstalk.hourly/logrotate.elasticbeanstalk.tomcat8.conf"
-  		sed -i 's/weekly/daily/g' $vFIL
-		sed -i 's/rotate /#rotate\nrotate 120/g' $vFIL
-		sed -i 's/#compress/compress/g' $vFIL
-	#/etc/logrotate.d/tomcat8daily
-	vFIL="/etc/logrotate.d/tomcat8daily"
-		sed -i 's/weekly/daily/g' $vFIL
-		sed -i 's/rotate /#rotate\nrotate 120/g' $vFIL
-		sed -i 's/#compress/compress/g' $vFIL	
+  vSTI01="/etc/logrotate.d"
+  vSTI02="/etc/logrotate.elasticbeanstalk.hourly"
+  vTOMCAT01="$vSTI01/tomcat $vSTI01/tomcat7 $vSTI01/tomcat8"
+  vTOMCAT02="$vSTI01/tomcat8daily $vSTI01/logrotatetomcat*"
+  vTOMCAT03="$vSTI01/logrotate.elasticbeanstalk.tomcat8.conf"
+  vTOMCAT04="$vSTI02/logrotate.elasticbeanstalk.tomcat8.conf"
+  vFILER="$vTOMCAT01 $vTOMCAT02 $vTOMCAT03 $vTOMCAT04"
+
+  for vFIL in $vFILER
+  do
+    if [ -f $vFIL ]
+    then
+      printf ".. (EXIST)\t ..modifying \"$vFIL\" ..\n"
+	  vDATE=`date '+%Y%m%d'`	  
+	  cp -p $vFIL /tmp/$vFIL.$vDATE
+      #sed 's/^weekly/daily/g; s/^rotate 4/rotate 120/g; s/^#compress/compress/g' $vFIL
+      sed -i 's/^size 10M/size 1G/g; s/^rotate [45]/rotate 120/g; s/^#compress/compress/g; s/^dateext/#dateext/g; s/^dateformat/#dateformat/g' $vFIL
+    else
+      printf ".. (ERR)\t ..modifying \"$vFIL\" . File doesn't exist..\n"
+    fi
+  done  
   EOH
 end
