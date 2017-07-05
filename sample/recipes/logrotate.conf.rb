b0d4360e81748ea0223085def1afe14d898d55c3
Djura <dk@storebox.com>
2017-07-05 14:38:11 +0200

    /DK

--- sample/recipes/logrotate.conf.rb
+++ sample/recipes/logrotate.conf.rb
@@ -4,7 +4,7 @@ bash "change Logrotate.conf" do
 	vDATE=`date '+%Y%m%d'`
 	#/etc/logrotate.conf"
 	vFIL="/etc/logrotate.conf"
-    cp -p $vFIL $vFIL.$vDATE
+	cp -p $vFIL $vFIL.$vDATE
 	sed -i 's/^weekly/daily/g; s/^rotate [45]/rotate 120/g; s/^#compress/compress/g' $vFIL
   EOH
 end
--- sample/recipes/logrotate.tomcat.rb
+++ sample/recipes/logrotate.tomcat.rb
@@ -1,26 +1,26 @@
 bash "change Logrotate.tomcat-conf" do
-  user "root"
-  code <<-EOH
-  vSTI01="/etc/logrotate.d"
-  vSTI02="/etc/logrotate.elasticbeanstalk.hourly"
-  vTOMCAT01="$vSTI01/tomcat $vSTI01/tomcat7 $vSTI01/tomcat8"
-  vTOMCAT02="$vSTI01/tomcat8daily $vSTI01/logrotatetomcat*"
-  vTOMCAT03="$vSTI01/logrotate.elasticbeanstalk.tomcat8.conf"
-  vTOMCAT04="$vSTI02/logrotate.elasticbeanstalk.tomcat8.conf"
-  vFILER="$vTOMCAT01 $vTOMCAT02 $vTOMCAT03 $vTOMCAT04"
+	user "root"
+	code <<-EOH
+	vSTI01="/etc/logrotate.d"
+	vSTI02="/etc/logrotate.elasticbeanstalk.hourly"
+	vTOMCAT01="$vSTI01/tomcat $vSTI01/tomcat7 $vSTI01/tomcat8"
+	vTOMCAT02="$vSTI01/tomcat8daily $vSTI01/logrotatetomcat*"
+	vTOMCAT03="$vSTI01/logrotate.elasticbeanstalk.tomcat8.conf"
+	vTOMCAT04="$vSTI02/logrotate.elasticbeanstalk.tomcat8.conf"
+	vFILER="$vTOMCAT01 $vTOMCAT02 $vTOMCAT03 $vTOMCAT04"
 
-  for vFIL in $vFILER
-  do
-    if [ -f $vFIL ]
-    then
-      printf ".. (EXIST)\t ..modifying \"$vFIL\" ..\n"
-	  vDATE=`date '+%Y%m%d'`	  
-	  cp -p $vFIL /tmp/$vFIL.$vDATE
-      #sed 's/^weekly/daily/g; s/^rotate 4/rotate 120/g; s/^#compress/compress/g' $vFIL
-      sed -i 's/^size 10M/size 1G/g; s/^rotate [45]/rotate 120/g; s/^#compress/compress/g; s/^dateext/#dateext/g; s/^dateformat/#dateformat/g' $vFIL
-    else
-      printf ".. (ERR)\t ..modifying \"$vFIL\" . File doesn't exist..\n"
-    fi
-  done  
-  EOH
+	for vFIL in $vFILER
+	do
+	if [ -f $vFIL ]
+	then
+		printf ".. (EXIST)\t ..modifying \"$vFIL\" ..\n"
+		vDATE=`date '+%Y%m%d'`		
+		cp -p $vFIL /tmp/$vFIL.$vDATE
+		#sed 's/^weekly/daily/g; s/^rotate 4/rotate 120/g; s/^#compress/compress/g' $vFIL
+		sed -i 's/^size 10M/size 1G/g; s/^rotate [45]/rotate 120/g; s/^#compress/compress/g; s/^dateext/#dateext/g; s/^dateformat/#dateformat/g' $vFIL
+	else
+		printf ".. (ERR)\t ..modifying \"$vFIL\" . File doesn't exist..\n"
+	fi
+	done	
+EOH
 end
