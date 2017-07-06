56b5010c1c189bb73ce6d4d332bbfffd9bbb802f
Djura <dk@storebox.com>
2017-07-06 15:36:03 +0200

    /DK

--- sample/recipes/addDNS.rb
+++ sample/recipes/addDNS.rb
@@ -1,10 +1,9 @@
-bash "change Logrotate.conf" do
+bash "adding service addDNS" do
 	user "root"
 	code <<-EOH
 
 	touch /etc/rc.d/init.d/addDNS
 cat <<EOF | sudo tee /etc/rc.d/init.d/addDNS
-
 #!/bin/bash
 #
 # /etc/init.d/addDNS
@@ -22,12 +21,59 @@ cat <<EOF | sudo tee /etc/rc.d/init.d/addDNS
 ## cat /etc/resolv.conf
 
 # source function library
+. /etc/rc.d/init.d/functions
+
+function vINIT {
+	#Remove nameserver 8.8.8.8
+	#Remove nameserver 8.8.4.4
+	sed -i "/Added by service addDNS/d" /etc/resolv.conf 
+	sed -i "/Added by systemctl addDNS/d" /etc/resolv.conf 	
+	sed -i "/8.8.8.8/d" /etc/resolv.conf 
+	sed -i "/8.8.4.4/d" /etc/resolv.conf 
+}
+
+start() 
+{
+	#Add nameserver 8.8.8.8
+	#Add nameserver 8.8.4.4		
+	DNS1="8.8.8.8"
+	DNS2="8.8.4.4"
+	vINIT
+	printf "# Added by service addDNS \n" >> /etc/resolv.conf
+	printf "nameserver $DNS1 \n" >> /etc/resolv.conf
+	printf "nameserver $DNS2 \n" >> /etc/resolv.conf
+}
+
+stop() 
+{
+	#Remove nameserver 8.8.8.8
+	#Remove nameserver 8.8.4.4
+	echo "addDNS - stopper"
+	vINIT	
+}
+
+case "$1" in
+	start)
+		start
+		;;
+	stop)
+		stop
+		;;
+	restart)
+		stop
+		start
+		;;
+	*)
+		echo $"Usage: $0 {start|stop|restart}"
+		;;
+esac
+exit
 EOF
 
 #chmod 755 /etc/rc.d/init.d/addDNS
 #chkconfig --add addDNS
 #chkconfig addDNS on
 #service addDNS start
-	
+
   EOH
 end
