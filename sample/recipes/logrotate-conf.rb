df2407ebe957c39658c0be9515e64cafda63e045
Djura <dk@storebox.com>
2017-07-07 12:35:30 +0200

    /DK

--- sample/recipes/logrotate-conf.rb
+++ sample/recipes/logrotate-conf.rb
@@ -5,6 +5,7 @@ bash "change Logrotate.conf" do
 	#/etc/logrotate.conf
 	vFIL="/etc/logrotate.conf"
 	cp -p $vFIL $vFIL.chef.$vDATE
-	sed -i 's/^weekly/daily/g; s/^rotate [0123456789]/rotate 120/g; s/^#compress/compress/g' $vFIL
+	#sed -i 's/^weekly/daily/g; s/^rotate [0123456789]/rotate 120/g; s/^#compress/compress/g' $vFIL
+	sed -i 's/^weekly/daily/g; s/^rotate.*/rotate 120/g; s/^#compress/compress/g' $vFIL
   EOH
 end
